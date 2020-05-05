b3db9fbdc11dce5623e523d5b6fb48f22bf3e2f7
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-05-04 22:29:54 -0300

    90% done

--- src/components/ranking/ProfileConsultor.jsx
+++ src/components/ranking/ProfileConsultor.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import "./consultorProfile.css"
 
 class ProfileConsulter extends React.Component {
   
   render() {
     return (
       <div>
-        <p>{this.props.match.params.name}</p>
-        <p>{this.props.match.params.porcentageMonth}</p>
-        <p>{this.props.match.params.porcentageWeek}</p>
-        <p>{this.props.match.params.porcentageday}</p>
-        <p>{this.props.match.params.risk}</p>
-        <img src={`https://avatars2.githubusercontent.com/u/${this.props.match.params.imagePath}`} />
+        <div className="card-container">
+        <div class="image-container">
+				<img src="https://ca.slack-edge.com/TMDDFEPFU-USVFNTCCA-d56f329846a9-512" width="60px" />
+			</div>
+        
+          <div class="lower-container">
+            <div>
+              <p>Nome: {this.props.match.params.name}</p>
+              <p>Porcentagem no mês: {this.props.match.params.porcentageMonth}</p>
+              <p>Porcentagem na samana: {this.props.match.params.porcentageWeek}</p>
+              <p>Porcentagem no dia: {this.props.match.params.porcentageday}</p>
+              <p>Risco: {this.props.match.params.risk}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
